644f3534a25dba1a44fe6493673e25f4e96e0070
Shaghayegh Sadeghi <41548734+sshaghayeghs@users.noreply.github.com>
2024-05-01 12:10:43 -0400

    Update Embedding.py

--- Embedding.py
+++ Embedding.py
@@ -1,116 +1,91 @@
-import torch
-from transformers import AutoModel, AutoTokenizer
-import pandas as pd
 import numpy as np
+import pandas as pd
+import torch
+from transformers import (
+    AutoModel, AutoTokenizer, LlamaModel, LlamaTokenizer, RobertaModel, RobertaTokenizer,
+    BertModel, BertTokenizer, GPT2TokenizerFast, GPT2LMHeadModel, AutoModelForCausalLM
+)
 from tqdm import tqdm
 import openai
 from sentence_transformers import SentenceTransformer
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from peft import PeftModel, PeftConfig
-from angle_emb import Prompts
 import deepchem as dc
 from rdkit import Chem
-import pandas as pd
-class Embedding:
-    def __init__(self):
+from huggingface_hub import HfFolder
+class EmbeddingExtractor:
+    def __init__(self, hf_token=None, api_key=None):
+        self.hf_token = hf_token
+        self.api_key = api_key
         self.models = {
-            "simcse": self.simcse_embedding,
-            "chatgpt": self.chatgpt_embedding,
-            "sbert": self.sbert_embedding,
-            "anglebert": self.anglebert_embedding, 
-            "anglellama": self.angle_llama_embedding,  
-            "mol2vec": self.mol2vec_embedding,
-            "morgan": self.morgan_embedding
-
+            "llama2": ("meta-llama/Llama-2-7b-hf", LlamaModel, LlamaTokenizer),
+            "molformer": ("ibm/MoLFormer-XL-both-10pct", AutoModel, AutoTokenizer, True),  # Added a flag for trust_remote_code
+            "chemberta": ("DeepChem/ChemBERTa-10M-MLM", RobertaModel, RobertaTokenizer),
+            "bert": ("bert-base-uncased", BertModel, BertTokenizer),
+            "roberta_zinc": ("entropy/roberta_zinc_480m", RobertaModel, RobertaTokenizer),
+            "gpt2": ("entropy/gpt2_zinc_87m", GPT2LMHeadModel, GPT2TokenizerFast),
+            "roberta": ("FacebookAI/roberta-base", RobertaModel, RobertaTokenizer),
+            "simcse": ("princeton-nlp/sup-simcse-bert-base-uncased", AutoModel, AutoTokenizer),
+            "anglebert": ("SeanLee97/angle-bert-base-uncased-nli-en-v1", AutoModel, AutoTokenizer),
+            "sbert": SentenceTransformer("all-MiniLM-L6-v2"),
+            "mol2vec": dc.feat.Mol2VecFingerprint(),
+            "morgan": dc.feat.CircularFingerprint(size=2048, radius=3)
         }
-        self.model_name = "text-embedding-3-small"  # Replace with the actual model name
+        if hf_token:
+            self.authenticate_huggingface(hf_token)
 
-    def chatgpt_embedding(self, texts, api_key):
-        openai.api_key = api_key
+    def authenticate_huggingface(self, token):
+        HfFolder.save_token(token)
+
+    def load_model_tokenizer(self, model_key):
+        if model_key in self.models:
+            item = self.models[model_key]
+            if isinstance(item, tuple):
+                if len(item) == 4:
+                    path, model_cls, tokenizer_cls, trust_remote = item
+                    model = model_cls.from_pretrained(path, trust_remote_code=trust_remote)
+                    tokenizer = tokenizer_cls.from_pretrained(path, trust_remote_code=trust_remote)
+                else:
+                    path, model_cls, tokenizer_cls = item
+                    model = model_cls.from_pretrained(path)
+                    tokenizer = tokenizer_cls.from_pretrained(path)
+                if model_key == "llama2":
+                    tokenizer.pad_token = tokenizer.eos_token
+                return model, tokenizer
+            else:
+                return item, None  # For feature extractors like 'morgan'
+        else:
+            raise ValueError(f"Unsupported model key: {model_key}")
+
+    def compute_embeddings(self, texts, model, tokenizer):
+        MAX_LENGTH = 512
         embeddings = []
-        for text in tqdm(texts):
-            try:
-                response = openai.Embedding.create(
-                    model=self.model_name,
-                    input=text)
-                embedding = [item["embedding"] for item in response["data"]]
-                embeddings.append(np.array(embedding[0]))
-            except Exception as e:
-                print(f"Error in processing text: {text}. Error: {e}")
-                embeddings.append(np.zeros(768))  # Assuming embedding size is 768, adjust if different
-        return embeddings
-    
-    def simcse_embedding(self, texts):
-        tokenizer = AutoTokenizer.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
-        model = AutoModel.from_pretrained("princeton-nlp/sup-simcse-bert-base-uncased")
-
-        inputs = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
-
-        with torch.no_grad():
-            embeddings = model(**inputs, output_hidden_states=True, return_dict=True).pooler_output
-
-        embeddings_cpu = embeddings.cpu()
-        numpy_embeddings = embeddings_cpu.numpy()
-        return numpy_embeddings
-    def anglebert_embedding(self, texts):
-        model_id = 'SeanLee97/angle-bert-base-uncased-nli-en-v1'
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        model = AutoModel.from_pretrained(model_id).cuda()
-
-        embeddings = []
-        for text in tqdm(texts):
-            tok = tokenizer([text], return_tensors='pt')
-            for k, v in tok.items():
-                tok[k] = v.cuda()
-
+        if tokenizer:
+            inputs = tokenizer(texts, return_tensors="pt", padding=True, truncation=True, max_length=MAX_LENGTH)
             with torch.no_grad():
-                hidden_state = model(**tok).last_hidden_state
-                vec = (hidden_state[:, 0] + torch.mean(hidden_state, dim=1)) / 2.0
-                embeddings.append(vec.cpu().numpy())
-
-        return np.vstack(embeddings)
-    def angle_llama_embedding(self, texts):
-        peft_model_id = 'SeanLee97/angle-llama-7b-nli-v2'
-        config = PeftConfig.from_pretrained(peft_model_id)
-        tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
-        base_model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path).bfloat16().cuda()
-        model = PeftModel.from_pretrained(base_model, peft_model_id).cuda()
-
-        embeddings = []
-        for text in tqdm(texts):
-            decorated_text = Prompts.A.format(text=text)
-            tok = tokenizer([decorated_text], return_tensors='pt')
-            for k, v in tok.items():
-                tok[k] = v.cuda()
-
-            with torch.no_grad():
-                vec = model(output_hidden_states=True, **tok).hidden_states[-1][:, -1].float().detach().cpu().numpy()
-                embeddings.append(vec)
-
-        return np.vstack(embeddings)
+                outputs = model(**inputs, output_hidden_states=True)
+                full_embeddings = outputs.hidden_states[-1]
+                mask = inputs['attention_mask']
+                embeddings = ((full_embeddings * mask.unsqueeze(-1)).sum(1) / mask.sum(-1).unsqueeze(-1))
+            return embeddings.detach().cpu().numpy()
+        else:  # Handle non-language models
+            return model.featurize(texts)  # Assuming texts are suitable for the model (e.g., SMILES strings for 'morgan')
 
     def sbert_embedding(self, texts):
         model = SentenceTransformer("all-MiniLM-L6-v2")
         embeddings = model.encode(texts)
         return embeddings
-        
-    def mol2vec_embedding(self, texts):
-        featurizer = dc.feat.Mol2VecFingerprint()
-        embeddings = featurizer.featurize(texts)
-        return embeddings
 
-    def morgan_embedding(self, texts):
-        featurizer = dc.feat.CircularFingerprint(size=2048, radius=3)
-        embeddings = featurizer.featurize(texts)
-        return embeddings
-        
-
-    def get_embeddings(self, model_name, texts, api_key=None):
-        if model_name in self.models:
-            if model_name == "chatgpt" and api_key:
-                return self.models[model_name](texts, api_key)
-            return self.models[model_name](texts)
+    def extract_features(self, texts, model_key):
+        if model_key in self.models:
+            if model_key == 'sbert':
+                embeddings = self.sbert_embedding(texts)
+            elif 'openai' in model_key:
+                embeddings = self.get_embeddings_from_openai(texts)
+            else:
+                model, tokenizer = self.load_model_tokenizer(model_key)
+                if tokenizer is None:  # Indicates a feature extractor like 'morgan'
+                    embeddings = model.featurize(texts)  # Assuming texts are SMILES strings for chemical features
+                else:
+                    embeddings = self.compute_embeddings(texts, model, tokenizer)
+            return pd.DataFrame(embeddings)
         else:
-            raise ValueError(f"Model {model_name} not supported.")
-
+            raise ValueError(f"Unsupported model key: {model_key}")
